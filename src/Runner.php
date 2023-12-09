82fac9a58e818dc019965103b3663539b97e3743
Ushkevich Oleg <olegserbat@yandex.ru>
2023-12-09 17:27:26 +0300

    add some librery

---
+++ src/Runner.php
@@ -0,0 +1,11 @@
+<?php
+namespace Hexlet\Php\Runner;
+
+function run()
+{
+$collection = collect(['taylor', 'abigail', null])->map(function ($name) {
+return strtoupper($name);
+});
+
+return $collection;
+}
