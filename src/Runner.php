f73a4a57de5ac3cad2fc21dc6f0033c80b3195db
olegserbat <olegserbat@yandex.ru>
2024-04-02 22:05:21 +0300

    step 1 gitHab Active

--- src/Runner.php
+++ src/Runner.php
@@ -1,11 +1,3 @@
 <?php
-namespace HexletGit\Runner;
 
-function run()
-{
-$collection = collect(['taylor', 'abigail', null])->map(function ($name) {
-return strtoupper($name);
-});
-
-return $collection;
-}
+echo 'Hellow world!';
